5d51e531c361b68a7f922eccdcced448e3599610
unknown <mainulisraf163@gmail.com>
2023-10-31 11:46:31 +0600

    query

---
+++ query/index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+  const id = req.query.id;
+  const name = req.query.name;
+  res.send(`student id is ${id} and name ${name}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});

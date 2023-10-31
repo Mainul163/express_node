7026c849b39a32379b4ed14c27f392cf6ac45a2d
unknown <mainulisraf163@gmail.com>
2023-10-31 16:53:24 +0600

    header add

--- query/index.js
+++ query/index.js
@@ -8,6 +8,25 @@ app.get("/", (req, res) => {
   res.send(`student id is ${id} and name ${name}`);
 });
 
+app.get("/user", (req, res) => {
+  const id = req.query.id;
+
+  res.send(`student id is ${id} and name `);
+});
+
+app.get("/login/userId/:id/userAge/:age", (req, res) => {
+  const id = req.params.id;
+  const age = req.params.age;
+  res.send(`student id is ${id} and name ${age}`);
+});
+
+app.get("/student", (req, res) => {
+  const id = req.header("id");
+  const name = req.header("name");
+
+  res.send(`student id is ${id} and name ${name}`);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });

3067d4edd402ec70afe6ca32797bbddba06f6760
unknown <mainulisraf163@gmail.com>
2023-11-05 18:06:03 +0600

    environment setup

---
+++ environment/index.js
@@ -0,0 +1,15 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("hello new");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
+
+//install  npm i dotenv

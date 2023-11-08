4dc5b3e693256200c96e72fbf1fe5ac9a7d89b22
unknown <mainulisraf163@gmail.com>
2023-11-08 15:23:35 +0600

    get,put,post method done

--- rest-api/controllers/users.controller.js
+++ rest-api/controllers/users.controller.js
@@ -1,7 +1,32 @@
-const users = require("../model/user.model");
+let users = require("../model/user.model");
+const { v4: uuidv4 } = require("uuid");
 
+// get method
 const getAllUsers = (req, res) => {
   res.status(200).json({ users });
 };
+// post method
+const createAlluser = (req, res) => {
+  const newuser = {
+    id: uuidv4(),
+    username: req.body.username,
+    email: req.body.email,
+  };
+  users.push(newuser);
+  res.status(200).json(users);
+};
 
-module.exports = { getAllUsers };
+//update method
+
+const updateUser = (req, res) => {
+  const userid = req.params.id;
+  const { username, email } = req.body;
+  users
+    .filter((user) => user.id === userid)
+    .map((selecteduser) => {
+      (selecteduser.username = username), (selecteduser.email = email);
+    });
+
+  res.status(200).json(users);
+};
+module.exports = { getAllUsers, createAlluser, updateUser };
--- rest-api/router/user.route.js
+++ rest-api/router/user.route.js
@@ -1,7 +1,12 @@
-const { getAllUsers } = require("../controllers/users.controller");
+const {
+  getAllUsers,
+  createAlluser,
+  updateUser,
+} = require("../controllers/users.controller");
 
 const router = require("express").Router();
 
 router.get("/", getAllUsers);
-
+router.post("/", createAlluser);
+router.put("/:id", updateUser);
 module.exports = router;
